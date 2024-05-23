8fcd1f454f11d6918fda7ec9b9d799c6a3d8200f
akiraa710 <21211@students.mrgs,school.nz>
2024-05-23 13:46:06 +1200

    put variables into seperate classes and other python files

---
+++ app.py
@@ -0,0 +1,14 @@
+from tkinter import *
+from app_settings import *
+
+class App():
+
+    def __init__(self):
+        window = Tk()
+        window.geometry("1440x1024")
+        window.title("WalletWise")
+        navBar=Frame(window, bg="#1b2e38", width=385, height=1024).pack(side=LEFT)
+
+        searchBar=Frame(window, bg="#D9D9D9", width=997, height=102).pack(side=TOP, pady=25)
+
+        window.mainloop()
---
+++ app_settings.py
@@ -0,0 +1,6 @@
+class App():
+
+    def __init__(self):
+        window = Tk()
+        window.geometry("1440x1024")
+        window.title("WalletWise")
