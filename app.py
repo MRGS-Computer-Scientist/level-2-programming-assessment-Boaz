5b232d351972c746b542881e6f2ddc39b86ce76e
akiraa710 <21211@students.mrgs,school.nz>
2024-05-24 12:33:42 +1200

    added self. and added exit button

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 from tkinter import *
 from app_settings import *
+from os import *
 
 class App():
 
     def __init__(self):
-        window = Tk()
-        window.geometry("1440x1024")
-        window.title("WalletWise")
-        navBar=Frame(window, bg="#1b2e38", width=385, height=1024).pack(side=LEFT)
+        self.window = Tk()
+        self.window.geometry("1440x1024")
+        self.window.title("WalletWise")
 
-        searchBar=Frame(window, bg="#D9D9D9", width=997, height=102).pack(side=TOP, pady=25)
+        self.navBar=Frame(self.window, bg="#1b2e38", width=385, height=1024).pack(side=LEFT)
+        self.searchBar=Frame(self.window, bg="#D9D9D9", width=997, height=102).pack(side=TOP, pady=25)
 
-        window.mainloop()
+        self.exit_button = Button(self.window, text="Exit", height=1, width=5, bg='red', command=self.exit)
+        self.exit_button.place(x = 0, y = 0)
+
+
+
+
+        self.window.mainloop()
+    
+    def exit(self):
+        self.window.destroy()
--- app_settings.py
+++
@@ -1,6 +0,0 @@
-class App():
-
-    def __init__(self):
-        window = Tk()
-        window.geometry("1440x1024")
-        window.title("WalletWise")
