8ffbcc466b151c06b9979e1cb92c380151c0f1d1
akiraa710 <21211@students.mrgs,school.nz>
2024-05-28 09:29:45 +1200

    changed exit button to home button and placed in correct spot, doesn't function as home button but exit button. added frame for my logo in the top left

--- app.py
+++ app.py
@@ -12,13 +12,18 @@ class App():
         self.navBar=Frame(self.window, bg="#1b2e38", width=385, height=1024).pack(side=LEFT)
         self.searchBar=Frame(self.window, bg="#D9D9D9", width=997, height=102).pack(side=TOP, pady=25)
 
-        self.exit_button = Button(self.window, text="Exit", height=1, width=5, bg='red', command=self.exit)
-        self.exit_button.place(x = 0, y = 0)
+
+        self.logo=Frame()
+        self.logo.place(x=20,y=22)
+
+
+        self.home_button = Button(self.window, text="Home", height=5, width=8, command=self.home)
+        self.home_button.place(x=20,y=148)
 
 
 
 
         self.window.mainloop()
     
-    def exit(self):
+    def home(self):
         self.window.destroy()
