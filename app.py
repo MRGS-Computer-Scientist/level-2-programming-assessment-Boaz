6ab4ee839f21a7862edc56ff7e87a38b23f5a3b4
akiraa710 <21211@students.mrgs,school.nz>
2024-06-13 13:30:02 +1200

    created working home button and start to code budget button but LINE 50 DOES NOT WORK UYET PLEASE FIX

--- app.py
+++ app.py
@@ -1,60 +1,61 @@
 from tkinter import *
-from app_settings import *
-from os import *
+from app_settings import *  # Ensure this module is available and correctly configured
+from os import path
 from PIL import ImageTk, Image
 
-class App():
+class App:
 
     def __init__(self):
         self.window = Tk()
         self.window.geometry("1440x1024")
         self.window.title("WalletWise")
 
-        #Navigation Bar to the left
-        self.navBar=Frame(self.window, bg="#1b2e38", width=385, height=1024).pack(side=LEFT)
-        
-        #@Search Bar up the top
-        self.searchBar=Frame(self.window, bg="#D9D9D9", width=997, height=102).pack(side=TOP, pady=25)
+        self.create_widgets()
+        self.window.mainloop()
 
-        #Logo top left
-        image = Image.open("images/WalletWise_logo.png")
-        photo = ImageTk.PhotoImage(image.resize((370, 120)))
+    def create_widgets(self):
+        # Navigation Bar to the left
+        self.navBar = Frame(self.window, bg="#1b2e38", width=385, height=1024)
+        self.navBar.pack(side=LEFT, fill=Y)
 
-        #load image 
-        self.home=Image.open("images/home.png")
-        home=ImageTk.PhotoImage(image.resize((5,5)))
+        # Search Bar up the top
+        self.searchBar = Frame(self.window, bg="#D9D9D9", width=997, height=102)
+        self.searchBar.pack(side=TOP, pady=25)
 
+        # Logo top left
+        image_path = path.join("images", "WalletWise_logo.png")
+        image = Image.open(image_path)
+        self.logo_photo = ImageTk.PhotoImage(image.resize((370, 120)))
 
+        self.logo_frame = Frame(self.window, width=344, height=102)
+        self.logo_frame.place(x=7, y=22)
 
-        self.mainframe=Frame(width=344, height=102)
-        self.mainframe.place(x=20,y=22)
+        self.logo = Label(self.logo_frame, image=self.logo_photo, bd=0, highlightthickness=0)
+        self.logo.pack(expand=True)
 
-        self.logo = Label(image=photo)
-        self.logo.place(x=6, y=15)
-
-        
-        # Load home.png image
-        home_image = Image.open("images/home.png")
-        self.home_photo = ImageTk.PhotoImage(home_image)
-
-        # Get the size of the image
-        width, height = home_image.size
-
-        # Create home button with the same size as the image
-        self.home_button = Button(self.window, image=self.home_photo, command=self.home, width=width, height=height)
-
+        # Load home image
+        home_image_path = path.join("images", "home_button.png")
+        home_image = Image.open(home_image_path)
+        home_image_resized = home_image.resize((200, 75), Image.LANCZOS)
+        self.home_photo = ImageTk.PhotoImage(home_image_resized)
 
         # Create home button
-        self.home_button = Button(self.window, image=self.home_photo, command=self.home, width=50, height=50)
-        self.home_button.image = self.home_photo
-        self.home_button.place(x=20, y=148)
+        self.home_button = Button(self.window, image=self.home_photo, command=self.home, bd=0, highlightthickness=0)
+        self.home_button.place(x=10, y=150)
 
-
-        
-
-
-
-        self.window.mainloop()
-    
     def home(self):
         self.window.destroy()
+
+
+        # Load Budget image
+        budget_button_path = path.join("images", "budget_button.png")
+        home_image = Image.open(budget_button_path)
+        home_image_resized = home_image.resize((200, 75), Image.LANCZOS)
+        self.home_photo = ImageTk.PhotoImage(home_image_resized)
+
+        # Create home button
+        self.home_button = Button(self.window, image=self.home_photo, command=self.home, bd=0, highlightthickness=0)
+        self.home_button.place(x=10, y=300)
+
+if __name__ == "__main__":
+    app = App()
